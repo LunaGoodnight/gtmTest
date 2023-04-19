fce0c5d1eb27b99be6a8409f886b3e0db700cb16
Luna Goodnight <luna.lee@nextfortune.tw>
2023-04-20 04:12:33 +0800

    [Add] init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import { useState } from "react";
+
+export const CuteButton = styled.button`
+  background: teal;
+`;
 
 function App() {
+  const [data, setData] = useState("cuteLuna");
+  const handleClick = () => {};
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>A website to practice gtm</p>
+        <CuteButton type="button" id="click-me">
+          Click Me
+        </CuteButton>
       </header>
     </div>
   );
